3eb34a1cf049455016ed351309a1857af2605824
Ishita Jaiswal <52828930+ishitajaiswal4m@users.noreply.github.com>
2021-03-15 12:15:50 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,10 @@
+class student {
+    int rn =101;
+    String sname="KIET";
+    public static void main(String args[]){
+    student st=new student();
+    System.out.println("roll no is"+st.rn);
+    }
+    }
+    
+    
